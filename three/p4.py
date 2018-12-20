ceee9c8b9b4564fb1494af8abd765b56dcf42e76
Jokgojegy <jingfeng361ur@126.com>
2018-12-20 09:58:22 +0800

    Python 12.19 (正则表达式) 贪婪与非贪婪“？”的使用规范

---
+++ three/p4.py
@@ -0,0 +1,17 @@
+import re
+
+a = 'pytho0python1pythonn2py3'
+#  "*"前的字符可以出现0次或者无限多次
+r = re.findall('python*', a)
+#  "+"前的字符最少出现一次或这无限多次
+s = re.findall('python+', a)
+#  “？”前的字符出现1次或者无限多次
+t = re.findall('python?', a)
+
+# 贪婪  与  非贪婪
+#   贪婪
+B = re.findall('python{1,2}', a)
+#   非贪婪
+b = re.findall('python{1,2}?', a)
+
+print(r)
